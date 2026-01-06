65228e818ef069ec443e496ec75ce03365864177
Rajkrishna <rajkrishnaranastudy@gmail.com>
2026-01-06 09:18:22 +0530

    Initial commit, add db folder, prisma and also bank-webhook express app setup

---
+++ apps/user-app/app/lib/actions/createOnRampTxn.ts
@@ -0,0 +1,29 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import prisma from "@repo/db/client";
+
+export async function createOnRampTransaction(amount: number, provider: string) {
+    const session = await getServerSession(authOptions);
+    const token = Math.random().toString();
+    const userId = session?.user.id;
+    if (!userId) {
+        return {
+            message: "User not logged in",
+        };
+    }
+
+    prisma.onRampTransaction.create({
+        data: {
+            userId,
+            amount,
+            provider,
+            status: "Processing",
+            startTime: new Date(),
+            token,
+        },
+    });
+
+    return {
+        message: "On ramp transaction added",
+    };
+}
